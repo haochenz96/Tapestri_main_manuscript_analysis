9e280026b10e0fed40cf5d4cd14402a981803333
haochenz96 <haochenz1996@gmail.com>
2024-05-08 21:13:00 -0400

    updated BPA-2 falcon solution and relevant analysis

--- 0_condor_pipeline/cleanup_snv_lists_and_make_heatmaps.py
+++ 0_condor_pipeline/cleanup_snv_lists_and_make_heatmaps.py
@@ -9,7 +9,7 @@ snv_lists_dir = Path("/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analy
 N=69
 PON_OCCURENCE_FREQUENCY=0.5
 threshold_sample_count = N * PON_OCCURENCE_FREQUENCY
-tumors_pon_f = "/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/supp1b_general_genetics/snv_blacklists/all_tumor_samples_N=69_composite_snv_sheet.csv"
+tumors_pon_f = "/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/supp1b_general_genetics/snv_black_whitelists/all_tumor_samples_N=69_composite_snv_sheet.csv"
 tumors_pon_df = pd.read_csv(tumors_pon_f, sep=',', header=0, index_col=0)
 
 snv_blacklist = tumors_pon_df[
@@ -63,39 +63,39 @@ manual_snv_blacklist = [
 snv_blacklist += manual_snv_blacklist
 snv_loci_blacklist = [x.rsplit(":", 1)[0] for x in snv_blacklist]
 
-# %% Blacklist the SNPs and SNVs
-# patients_of_interest = ["BPA-5-RSX"]
-output_dir = Path("/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/supp1b_general_genetics/all_vars_mut_prev=0.01/sc_heatmaps")
-output_dir.mkdir(exist_ok=True, parents=True)
-for p in snv_lists_dir.glob("**/*-voi.hz_curated.txt"):
-    # if not any([x in str(p) for x in patients_of_interest]):
-    #     continue
-    manual_snv = pd.read_csv(p, sep='\t', index_col=0, comment = '#')
-    manual_snv.fillna({'annotation': ''}, inplace=True)
+# # %% Blacklist the SNPs and SNVs
+# # patients_of_interest = ["BPA-5-RSX"]
+# output_dir = Path("/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/supp1b_general_genetics/all_vars_mut_prev=0.01/sc_heatmaps")
+# output_dir.mkdir(exist_ok=True, parents=True)
+# for p in snv_lists_dir.glob("**/*-voi.hz_curated.txt"):
+#     # if not any([x in str(p) for x in patients_of_interest]):
+#     #     continue
+#     manual_snv = pd.read_csv(p, sep='\t', index_col=0, comment = '#')
+#     manual_snv.fillna({'annotation': ''}, inplace=True)
     
-    # flag all loci in blacklist as "likely_artifact"
-    count = 0
-    blacklisted = []
-    for locus, row in manual_snv.iterrows():
-        if "germline" in row["annotation"]:
-            if locus.rsplit(":", 1)[0] in snp_loci_blacklist:
-                if not manual_snv.loc[locus, 'annotation'] == "likely_artifact":
-                    manual_snv.loc[locus, 'annotation'] = 'likely_artifact'
-                    count += 1
-                    # blacklisted.append(manual_snv.loc[locus, 'HGVSp'])
-                    blacklisted.append(locus)
-        else:
-            if locus.rsplit(":", 1)[0] in snv_loci_blacklist:
-                if not manual_snv.loc[locus, 'annotation'] == "likely_artifact":
-                    manual_snv.loc[locus, 'annotation'] = 'likely_artifact'
-                    count += 1
-                    # blacklisted.append(manual_snv.loc[locus, 'HGVSp'])
-                    blacklisted.append(locus)
-    print(f"""[INFO] Blacklisted {count} SNVs/SNPs in {p.stem}: 
-          {blacklisted}""")
-    # write output 
-    output_f = output_dir / (p.stem)
-    manual_snv.to_csv(output_f, sep='\t', index=True)
+#     # flag all loci in blacklist as "likely_artifact"
+#     count = 0
+#     blacklisted = []
+#     for locus, row in manual_snv.iterrows():
+#         if "germline" in row["annotation"]:
+#             if locus.rsplit(":", 1)[0] in snp_loci_blacklist:
+#                 if not manual_snv.loc[locus, 'annotation'] == "likely_artifact":
+#                     manual_snv.loc[locus, 'annotation'] = 'likely_artifact'
+#                     count += 1
+#                     # blacklisted.append(manual_snv.loc[locus, 'HGVSp'])
+#                     blacklisted.append(locus)
+#         else:
+#             if locus.rsplit(":", 1)[0] in snv_loci_blacklist:
+#                 if not manual_snv.loc[locus, 'annotation'] == "likely_artifact":
+#                     manual_snv.loc[locus, 'annotation'] = 'likely_artifact'
+#                     count += 1
+#                     # blacklisted.append(manual_snv.loc[locus, 'HGVSp'])
+#                     blacklisted.append(locus)
+#     print(f"""[INFO] Blacklisted {count} SNVs/SNPs in {p.stem}: 
+#           {blacklisted}""")
+#     # write output 
+#     output_f = output_dir / (p.stem)
+#     manual_snv.to_csv(output_f, sep='\t', index=True)
 
 
 # %% Load data and plot heatmaps
@@ -103,8 +103,6 @@ import yaml
 import mosaic.io as mio
 import plotly.express as px
 from tea.plots import plot_snv_clone
-
-snv_analysis_dir = analysis_dir / "snv-cnv-combined"
 
 h5_dir = Path('/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled/fillout_h5')
 opt_nclones_dir = Path("/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled/falcon_solutions")
@@ -114,7 +112,7 @@ patient_info_f = "/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/
 with open(patient_info_f, 'r') as f:
     patient_info = yaml.safe_load(f)
 patient_names = patient_info.keys()
-
+patient_names = ["BPA-2", "BPA-3"]
 for patient_name in patient_names:
     if (output_dir / f"{patient_name}-DNA-heatmap.pdf").is_file():
         print(f'[INFO] Skipping {patient_name}.')
--- _lilac_compile_tapestri_data.sh
+++ _lilac_compile_tapestri_data.sh
@@ -1,55 +1,55 @@
-# COMPILED_DATA_DIR=/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled
-# mkdir -p ${COMPILED_DATA_DIR}/fillout_h5
-# mkdir -p ${COMPILED_DATA_DIR}/falcon_solutions
-# mkdir -p ${COMPILED_DATA_DIR}/manual_annotated_snv_lists
-# mkdir -p ${COMPILED_DATA_DIR}/cravat
+COMPILED_DATA_DIR=/lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled
+mkdir -p ${COMPILED_DATA_DIR}/fillout_h5
+mkdir -p ${COMPILED_DATA_DIR}/falcon_solutions
+mkdir -p ${COMPILED_DATA_DIR}/manual_annotated_snv_lists
+mkdir -p ${COMPILED_DATA_DIR}/cravat
 
-# ===== Caitlin_Aki cases =====
-CAITLIN_AKI_CASES=(M04 M07 M11 M12 M13 RA15_06 RA15_16 RA16_08 RA16_29 RA17_13 RA17_22 RA19_02 RA19_10 RA19_21 RA20_05 RA21_17) # N=15
-CAITLIN_AKI_h5_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/fillout_h5
-CAITLIN_AKI_falcon_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/falcon_results
-CAITLIN_AKI_annotated_snv_lists_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/manual_annotated_snv_lists
-for case_i in ${CAITLIN_AKI_CASES[@]}
-do
-    for i in $(find -L ${CAITLIN_AKI_h5_dir} -name ${case_i}*.patient_wide.genotyped.h5)
-    do
-        rsync -avzu -L $i ${COMPILED_DATA_DIR}/fillout_h5/
-    done
-    for i in $(find -L ${CAITLIN_AKI_h5_dir} -name ${case_i}*CRAVAT_output_cleaned.txt)
-    do
-        rsync -avzu -L $i ${COMPILED_DATA_DIR}/cravat
-    done
-
-    # for i in $(find -L ${CAITLIN_AKI_falcon_dir}/ \
-    #     -name "${case_i}*assignment.updated.csv" -o -name *unique*clone_profile*.csv -o -name *png)
-    # do
-    #     rsync -avzu -L $i ${COMPILED_DATA_DIR}/falcon_solutions/
-    # done
-
-    # for snv_f in $(find -L ${CAITLIN_AKI_annotated_snv_lists_dir}/ -name ${case_i}*.txt)
-    # do
-    #     rsync -avzu -L $snv_f ${COMPILED_DATA_DIR}/manual_annotated_snv_lists/
-    # done
-done
-
-
-# # ===== BRCA cases =====
-# BRCA_CASES=(BPA-1 BPA-2 BPA-3 BPA-5)
-# BRCA_snv_pipeline_output_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/fillout_h5/
-# BRCA_falcon_output_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/falcon_solutions/
-# BRCA_annotated_snv_lists_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/manual_annotated_snv_lists/
-
-# for case_i in ${BRCA_CASES[@]}
+# # ===== Caitlin_Aki cases =====
+# CAITLIN_AKI_CASES=(M04 M07 M11 M12 M13 RA15_06 RA15_16 RA16_08 RA16_29 RA17_13 RA17_22 RA19_02 RA19_10 RA19_21 RA20_05 RA21_17) # N=15
+# CAITLIN_AKI_h5_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/fillout_h5
+# CAITLIN_AKI_falcon_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/falcon_results
+# CAITLIN_AKI_annotated_snv_lists_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_compiled/manual_annotated_snv_lists
+# for case_i in ${CAITLIN_AKI_CASES[@]}
 # do
-# 	rsync -avzu -L ${BRCA_snv_pipeline_output_dir} ${COMPILED_DATA_DIR}/fillout_h5/
-# 	rsync -avzu -L ${BRCA_falcon_output_dir} ${COMPILED_DATA_DIR}/falcon_solutions/
-# 	rsync -avzu -L ${BRCA_annotated_snv_lists_dir} ${COMPILED_DATA_DIR}/manual_annotated_snv_lists/
+#     for i in $(find -L ${CAITLIN_AKI_h5_dir} -name ${case_i}*.patient_wide.genotyped.h5)
+#     do
+#         rsync -avzu -L $i ${COMPILED_DATA_DIR}/fillout_h5/
+#     done
+#     for i in $(find -L ${CAITLIN_AKI_h5_dir} -name ${case_i}*CRAVAT_output_cleaned.txt)
+#     do
+#         rsync -avzu -L $i ${COMPILED_DATA_DIR}/cravat
+#     done
+
+#     # for i in $(find -L ${CAITLIN_AKI_falcon_dir}/ \
+#     #     -name "${case_i}*assignment.updated.csv" -o -name *unique*clone_profile*.csv -o -name *png)
+#     # do
+#     #     rsync -avzu -L $i ${COMPILED_DATA_DIR}/falcon_solutions/
+#     # done
+
+#     # for snv_f in $(find -L ${CAITLIN_AKI_annotated_snv_lists_dir}/ -name ${case_i}*.txt)
+#     # do
+#     #     rsync -avzu -L $snv_f ${COMPILED_DATA_DIR}/manual_annotated_snv_lists/
+#     # done
 # done
 
-# # rename BPA-2 and BPA-5's names
-# cd /lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled
-# rename --verbose BPA-2-IR BPA-2 */*
-# rename --verbose BPA-5-RSX BPA-5 */*
+
+# ===== BRCA cases =====
+BRCA_CASES=(BPA-1 BPA-2 BPA-3 BPA-5)
+BRCA_snv_pipeline_output_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/fillout_h5/
+BRCA_falcon_output_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/falcon_solutions/
+BRCA_annotated_snv_lists_dir=/lila/data/iacobuzc/haochen/Tapestri_batch2/batch2_data_BRCA_compiled/manual_annotated_snv_lists/
+
+for case_i in ${BRCA_CASES[@]}
+do
+	# rsync -avzu -L ${BRCA_snv_pipeline_output_dir} ${COMPILED_DATA_DIR}/fillout_h5/
+	rsync -avzu -L ${BRCA_falcon_output_dir} ${COMPILED_DATA_DIR}/falcon_solutions/
+	# rsync -avzu -L ${BRCA_annotated_snv_lists_dir} ${COMPILED_DATA_DIR}/manual_annotated_snv_lists/
+done
+
+# rename BPA-2 and BPA-5's names
+cd /lila/data/iacobuzc/haochen/Tapestri_main_manuscript_analysis/data_compiled
+rename --verbose BPA-2-IR BPA-2 */*
+rename --verbose BPA-5-RSX BPA-5 */*
 
 # mv fillout_h5/BPA-2.mpileup.renamed.h5 fillout_h5/BPA-2.patient_wide.genotyped.h5
 # mv fillout_h5/BPA-5.mpileup.renamed.h5 fillout_h5/BPA-5.patient_wide.genotyped.h5
